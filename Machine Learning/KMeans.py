193053387d014d2518881f940b499dbc1f4d29c9
pat <31299967+pcarderdev@users.noreply.github.com>
2021-08-05 17:44:09 -0400

    Update KMeans.py
    Notes on metric measurements

--- Machine Learning/KMeans.py
+++ Machine Learning/KMeans.py
@@ -32,5 +32,17 @@ def bench_k_means(estimator, name, data):
         metrics.adjusted_mutual_info_score(y, estimator.labels_),
         metrics.silhouette_score(data, estimator.labels_, metric='euclidean')))
 
+'''
+https://scikit-learn.org/stable/modules/clustering.html#clustering-evaluation
+Homogeneity score - each cluster contains only members of a single class (0.0-1.0) 
+Completeness score - all members of a given class are assigned to the same cluster (0.0-1.0)
+V_measure score - harmonic mean of homogeneity and completeness, B defaults to 1.0 (beta=)
+                    v = ((1 + B) x homogeneity x completeness) /
+                        (B x homogeneity + completeness)
+Adjusted Rand score - 
+Adjusted Mutual Info score - 
+Silhouette score - 
+'''
+
 clf = KMeans(n_clusters=k, init='random', n_init=10)
 bench_k_means(clf, "1", data)
